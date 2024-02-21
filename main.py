b1eeddb5828b9a8656e73e5afe6553cb03b0a0a4
webby813 <webbyteo02@gmail.com>
2024-02-21 23:24:56 +0800

    - First commit.

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from Functions.Search import search_emails
 from Functions.Delete import delete_emails
 from config import CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES, SEARCH_QUERY
 
-#main
+
 def main():
     # Create Gmail service
     gmail_service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-
     # Search for emails
     email_results = search_emails(gmail_service, SEARCH_QUERY)
-
     # Delete the emails
     delete_emails(gmail_service, email_results)
 
