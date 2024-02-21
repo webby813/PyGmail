cd47b9ef55b129e1320c0e3ea97ecdc47b7cd111
webby813 <webbyteo02@gmail.com>
2024-02-21 23:35:57 +0800

    - gitignore update

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from config import CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES, SEARCH_QUE
 def main():
     # Create Gmail service
     gmail_service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
     # Search for emails
     email_results = search_emails(gmail_service, SEARCH_QUERY)
+
     # Delete the emails
     delete_emails(gmail_service, email_results)
 
